e7a773394389e9ebc992235bb4e0c2a0cbe56eb1
Đào Đức Anh <daoducanh1000@gmail.com>
2026-08-30 15:04:13 +0700

    PY04004 — LỚP PHÂN SỐ - 2

---
+++ PY04004_LopPhanSo2.py
@@ -0,0 +1,37 @@
+class PhanSo:
+    def __init__(self,tu,mau):
+        self.tu = tu
+        self.mau = mau
+
+
+    def Output (self):
+        print(f"{self.tu:.0f}/{self.mau:.0f}")
+
+    def Rutgon (self):
+        gcd = UCLN(self.tu, self.mau)
+        self.tu //= gcd
+        self.mau //= gcd
+
+    def sum (self, p2):
+        sumTu = self.tu * p2.mau + p2.tu * self.mau
+        sumMau = self.mau * p2.mau
+        tong = PhanSo(sumTu, sumMau)
+        tong.Rutgon()
+
+        print(f"{tong.tu}/{tong.mau}")
+        
+
+def UCLN(a,b):
+    while b > 0:
+        tmp = a
+        a = b
+        b = tmp % a
+    return a
+
+
+arr = input().split()
+p1 = PhanSo(int(arr[0]), int(arr[1]))
+p2 = PhanSo(int(arr[2]), int(arr[3]))
+
+p1.sum(p2)
+
