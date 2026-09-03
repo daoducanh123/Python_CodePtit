3bacc7328428cd22de4a81a0f57744dd76d9932e
Đào Đức Anh <daoducanh1000@gmail.com>
2026-09-03 14:58:21 +0700

    ICPC0111_XOayMang

--- ICPC0111_XoayMang.py
+++ ICPC0111_XoayMang.py
@@ -1,16 +1,17 @@
-import sys
-input = sys.stdin.buffer.readline
+t = int(input())
+while t > 0:
 
-T = int(input())
-out = []
-
-for _ in range(T):
-    n, d = map(int, input().split())
+    listInput = list(map(int, input().split()))
+    n = listInput[0]
+    d = listInput[1]
+    
     a = list(map(int, input().split()))
-
-    d %= n
-
-    out.append(' '.join(map(str, a[d:])))
-    out[-1] += ' ' + ' '.join(map(str, a[:d]))
-
-sys.stdout.write('\n'.join(out))
+    
+    for i in range(d, len(a)):
+        print(a[i], end = " ")
+        
+    for i in range (0, d):
+        print(a[i], end = " ")
+    
+    print()
+    t-=1
