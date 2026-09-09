3887922117f4ef203dba3ef5b7d280917f3dfb0f
Đào Đức Anh <daoducanh1000@gmail.com>
2026-09-09 20:39:52 +0700

    d

--- BaiThiPython8_9/tmp.py
+++ BaiThiPython8_9/tmp.py
@@ -1,11 +1,8 @@
-import math
-
-def check (x,y):
-    return math.gcd (x,y) == 1
-
-l, r = map(int,input().split())
-for i in range(l, r-2):
-    for j in range(i+1, r-1):
-        for k in range(j+1, r):
-            if check(i,j) and check (i,k) and check(j,k):
-                print(f"({i}, {j}, {k})")
+s = input()
+# binary -> 8     001 010
+while len(s) % 3 != 0:
+    s = "0" + s
+    
+for i in range (0, len(s), 3):
+    stringGroup = s[i:i+3]
+    print(int(stringGroup,2),end=" ")
--- ICPC0106_DoiCoSo2.py
+++ ICPC0106_DoiCoSo2.py
@@ -23,25 +23,14 @@ while t > 0:
             c = 3
         case 16:
             c = 4
-
-    # 00 0011 -> len(s) = 6
-    # c = 4 -> 6 % 4 == 2
-    # Đếm số bit cần vá
-    padNum = 0
-    if len(s) % c != 0:
-        padNum = c-(len(s) % c)
-
-    # Vá
-    stringRes = ""
-    stringPadding = ""
-    for i in range (0,padNum):
-        stringPadding = stringPadding + "0"
-    stringRes = stringPadding + s
+    
+    while len(s) % c != 0:
+        s = "0" + s
 
     #Group đổi lần lượt    
     # 0000 0011 -> 3
-    for i in range (0, len(stringRes), c):
-        stringGroup = int(stringRes[i:i+c],2)
+    for i in range (0, len(s), c):
+        stringGroup = int(s[i:i+c],2)
         if b == 16:
             stringGroup = stringHex[stringGroup]
         print(stringGroup, end = "")
