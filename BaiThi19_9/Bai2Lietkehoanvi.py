fde8d8a2e3115db1804c634fa901f2d0e2fe3b24
Đào Đức Anh <daoducanh1000@gmail.com>
2026-09-21 10:46:37 +0700

    d

--- BaiThi19_9/Bai2Lietkehoanvi.py
+++ BaiThi19_9/Bai2Lietkehoanvi.py
@@ -1,16 +1,15 @@
+
+    
 import math
-from itertools import permutations
+from itertools import permutations  
 t = int(input())
-for _ in range(t):
+for _ in range (t):
     n = int(input())
-    
-    #in so luonng hoan vi
-    print(math.factorial(n))
-    #sinh hoan vi tu lon xuong
-    a = list(range(n, 0, -1))
-    
+    a = [] 
+    for i in range(n,0,-1):
+        a.append(i) 
+        
     for p in permutations(a):
-        print(''.join(map(str,p)),end = ' ')
-    print()
-    
-    
+       p = map(str,p)
+       res = "".join(p)
+       print (res)
